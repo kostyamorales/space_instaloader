ff39cf7a92af1b606a52081ec2bffd9ef03af0fe
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-22 22:14:05 +0300

    Update spacex_downloader.py

--- spacex_downloader.py
+++ spacex_downloader.py
@@ -1,16 +1,20 @@
 import requests
-import pathlib
-from photo_cropping import resizes_images
+from pathlib import Path
+from photo_cropping import resize_image
 
 
-def downloads_pictures(picture_url, file_name):
-    pathlib.Path("images").mkdir(parents=True, exist_ok=True)
-    response = requests.get(picture_url, verify=False)
-    response.raise_for_status()
-    file_path = 'images/' + file_name + '.jpg'
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-        resizes_images(file_path, file_name)
+def download_picture(images_url):
+    Path("images").mkdir(parents=True, exist_ok=True)
+    data_files = []
+    for number, url in enumerate(images_url, 1):
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        file_name = f'spacex_{number}'
+        file_path = Path(f'images/{file_name}.jpg')
+        data_files.append((file_path, file_name))
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+    return data_files
 
 
 def fetch_spacex_last_launch():
@@ -18,8 +22,11 @@ def fetch_spacex_last_launch():
     response = requests.get(url)
     response.raise_for_status()
     answer = response.json()
-    for number, picture_url in enumerate(answer['links']['flickr_images'], 1):
-        downloads_pictures(picture_url, 'spacex_' + str(number))
+    images_url = []
+    for picture_url in answer['links']['flickr_images']:
+        images_url.append(picture_url)
+    data_files = download_picture(images_url)
+    resize_image(data_files)
 
 
 if __name__ == '__main__':
