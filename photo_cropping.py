966d9722123f8b87b013f6e4b119e47f7502bea3
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-22 22:14:52 +0300

    Update photo_cropping.py

--- photo_cropping.py
+++ photo_cropping.py
@@ -1,29 +1,36 @@
 import pathlib
 from math import floor
 from PIL import Image
+from os import path
 
 
-def resizes_images(file_path, file_name):
-    try:
-        image = Image.open(file_path)
-        if image.width / image.height > 1.91:
-            image = crop_width_images(image)
-        if image.width / image.height < 0.8:
-            image = crop_height_images(image)
-        if image.mode != 'RGB':
-            image = image.convert('RGB')
-        image.thumbnail((1080, 1080), Image.ANTIALIAS)
-        image.save('images/' + file_name + '.jpg', format="JPEG")
-        if file_path[-4:] != '.jpg':
-            original_file = pathlib.Path(file_path)
-            original_file.unlink()
-    except Image.UnidentifiedImageError:
-        original_file = pathlib.Path(file_path)
-        original_file.unlink()
-        print(file_name, 'PIL.UnidentifiedImageError. File deleted.')
+def resize_image(data_files):
+    for data_file in data_files:
+        file_path, file_name = data_file
+        try:
+            image = Image.open(file_path)
+            if image.width / image.height > 1.91:
+                image = crop_width_image(image)
+            if image.width / image.height < 0.8:
+                image = crop_height_image(image)
+            if image.mode != 'RGB':
+                image = image.convert('RGB')
+            image.thumbnail((1080, 1080), Image.ANTIALIAS)
+            image.save('images/' + file_name + '.jpg', format="JPEG")
+            root, ext = path.splitext(file_path)
+            if ext != '.jpg':
+                deleted_file(file_path)
+        except Image.UnidentifiedImageError:
+            deleted_file(file_path)
+            print(file_name, 'PIL.UnidentifiedImageError. File deleted.')
 
 
-def crop_width_images(image):
+def deleted_file(file_path):
+    original_file = pathlib.Path(file_path)
+    original_file.unlink()
+
+
+def crop_width_image(image):
     extra_width = image.width - floor(image.height * 1.9)
     x1 = extra_width // 2
     x2 = image.width - x1
@@ -35,7 +42,7 @@ def crop_width_images(image):
     return image
 
 
-def crop_height_images(image):
+def crop_height_image(image):
     extra_height = image.height - image.width // 0.8
     x1 = 0
     x2 = image.width
