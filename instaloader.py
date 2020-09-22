943dd0f2824e65df451bbd43b60c5bb60b882815
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-22 22:15:34 +0300

    Update instaloader.py

--- instaloader.py
+++ instaloader.py
@@ -1,46 +1,47 @@
 from instabot import Bot
-from os import getenv, listdir, remove
+from os import getenv, listdir, remove, path
 from dotenv import load_dotenv
 import time
+from pathlib import Path
 from shutil import rmtree
-
-load_dotenv()
-
-INSTA_LOGIN = getenv('INSTA_LOGIN')
-INSTA_PASSWORD = getenv('INSTA_PASSWORD')
 
 
 def publication_pictures():
     bot = Bot()
-    bot.login(username=INSTA_LOGIN, password=INSTA_PASSWORD)
+    bot.login(username=getenv('INSTA_LOGIN'), password=getenv('INSTA_PASSWORD'))
     try:
-        with open("./upload_images.txt", "r", encoding="utf8") as file:
+        with open("upload_images.txt", "r", encoding="utf8") as file:
             posted_image_list = file.read().splitlines()
     except Exception:
         posted_image_list = []
-    collection = listdir('./images')
+    collection = listdir('images')
     for image in collection:
         if image in posted_image_list:
             continue
         else:
-            bot.upload_photo('./images/' + image)
+            bot.upload_photo(Path(f'images/{image}'))
             posted_image_list.append(image)
-            with open("./upload_images.txt", "a", encoding="utf8") as file:
-                file.write(image + "\n")
+            with open("upload_images.txt", "a", encoding="utf8") as file:
+                file.write(f'{image}\n')
         time.sleep(5)
     clean_directories()
 
 
+# instabot после публикации фотографии добавляет файлу суффикс '.REMOVE_ME'
+# Данная функция удаляет файлы, опубликованных фотографий.
 def clean_directories():
-    for file in listdir('./images'):
-        if file[-10:] == '.REMOVE_ME':
-            file_path = './images/' + file
+    for file in listdir('images'):
+        root, ext = path.splitext(file)
+        if ext == '.REMOVE_ME':
+            file_path = Path(f'images/{file}')
             remove(file_path)
 
 
 if __name__ == '__main__':
-    # удаляет папку './config', чтобы избежать исключения 
-    # assert cookie_username == self.username.lower() AssertionError
-    rmtree('./config')
+    load_dotenv()
+
+    # удаляет папку './config' при ее наличии. Без этого действия скрипт выдает исключение AssertionError
+    if path.isdir('config'):
+        rmtree('config')
 
     publication_pictures()
