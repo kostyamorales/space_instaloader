1a4025ee7ae3cca58db2fdc96e70e0e6e2e55c11
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-22 22:16:06 +0300

    Update hubble_collection_downloader.py

--- hubble_collection_downloader.py
+++ hubble_collection_downloader.py
@@ -1,26 +1,35 @@
 import requests
-import pathlib
-from photo_cropping import resizes_images
+from pathlib import Path
+from photo_cropping import resize_image
 import argparse
 
 
-def download_picture(picture_url, id_picture, pic_expansion):
-    pathlib.Path("images").mkdir(parents=True, exist_ok=True)
-    response = requests.get(picture_url, verify=False)
-    response.raise_for_status()
-    file_name = 'image_' + id_picture
-    file_path = 'images/image_' + id_picture + '.' + pic_expansion
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-        resizes_images(file_path, file_name)
+def download_picture(images_url):
+    Path("images").mkdir(parents=True, exist_ok=True)
+    data_files = []
+    for image in images_url:
+        id, url = image
+        pic_expansion = url.split('.')[-1]
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        file_name = f'image_{id}'
+        file_path = Path(f'images/{file_name}.{pic_expansion}')
+        data_files.append((file_path, file_name))
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+    return data_files
 
 
-def get_hubble_picture_url(id):
-    url = f'http://hubblesite.org/api/v3/image/{id}'
-    response = requests.get(url)
-    response.raise_for_status()
-    picture_url = 'https:' + response.json()['image_files'][-1]['file_url']
-    return picture_url
+def get_hubble_picture_url(id_pictures):
+    images_url = []
+    for id in id_pictures:
+        url = f'http://hubblesite.org/api/v3/image/{id}'
+        response = requests.get(url)
+        response.raise_for_status()
+        url_image = response.json()['image_files'][-1]['file_url']
+        url_image = f'https:{url_image}'
+        images_url.append((id, url_image))
+    return images_url
 
 
 def get_hubble_collection(collection_name):
@@ -28,11 +37,12 @@ def get_hubble_collection(collection_name):
     response = requests.get(url)
     response.raise_for_status()
     collection = response.json()
+    id_pictures = []
     for element in collection:
-        id_picture = str(element['id'])
-        picture_url = get_hubble_picture_url(id_picture)
-        pic_expansion = picture_url.split('.')[-1]
-        download_picture(picture_url, id_picture, pic_expansion)
+        id_pictures.append(str(element['id']))
+    images_url = get_hubble_picture_url(id_pictures)
+    data_files = download_picture(images_url)
+    resize_image(data_files)
 
 
 if __name__ == '__main__':
