985d1a8b31d44307357b60ea309e94de66394a56
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-24 11:51:46 +0300

    Update photo_cropping.py

--- photo_cropping.py
+++ photo_cropping.py
@@ -1,36 +1,36 @@
-import pathlib
+from pathlib import Path
 from math import floor
 from PIL import Image
 from os import path
 
 
-def resize_image(data_files):
-    for data_file in data_files:
-        file_path, file_name = data_file
+def resizes_images(path_name_pictures):
+    for path_name_picture in path_name_pictures:
+        picture_path, picture_name = path_name_picture
         try:
-            image = Image.open(file_path)
+            image = Image.open(picture_path)
             if image.width / image.height > 1.91:
-                image = crop_width_image(image)
+                image = crops_width_image(image)
             if image.width / image.height < 0.8:
-                image = crop_height_image(image)
+                image = crops_height_image(image)
             if image.mode != 'RGB':
                 image = image.convert('RGB')
             image.thumbnail((1080, 1080), Image.ANTIALIAS)
-            image.save('images/' + file_name + '.jpg', format="JPEG")
-            root, ext = path.splitext(file_path)
+            image.save(Path(f'images/{picture_name}.jpg'), format="JPEG")
+            root, ext = path.splitext(picture_path)
             if ext != '.jpg':
-                deleted_file(file_path)
+                deletes_file(picture_path)
         except Image.UnidentifiedImageError:
-            deleted_file(file_path)
-            print(file_name, 'PIL.UnidentifiedImageError. File deleted.')
+            deletes_file(picture_path)
+            print(picture_name, 'PIL.UnidentifiedImageError. File deleted.')
 
 
-def deleted_file(file_path):
-    original_file = pathlib.Path(file_path)
+def deletes_file(picture_path):
+    original_file = Path(picture_path)
     original_file.unlink()
 
 
-def crop_width_image(image):
+def crops_width_image(image):
     extra_width = image.width - floor(image.height * 1.9)
     x1 = extra_width // 2
     x2 = image.width - x1
@@ -42,7 +42,7 @@ def crop_width_image(image):
     return image
 
 
-def crop_height_image(image):
+def crops_height_image(image):
     extra_height = image.height - image.width // 0.8
     x1 = 0
     x2 = image.width
