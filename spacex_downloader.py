5d1cc88dd34e184d2bfec7807d4381bf47c8c08e
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-09-25 12:11:53 +0300

    Update spacex_downloader.py

--- spacex_downloader.py
+++ spacex_downloader.py
@@ -1,20 +1,6 @@
 import requests
-from pathlib import Path
-from photo_cropping import resizes_images
-
-
-def download_picture(images_url):
-    Path("images").mkdir(parents=True, exist_ok=True)
-    data_files = []
-    for number, url in enumerate(images_url, 1):
-        response = requests.get(url, verify=False)
-        response.raise_for_status()
-        file_name = f'spacex_{number}'
-        file_path = Path(f'images/{file_name}.jpg')
-        data_files.append((file_path, file_name))
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
-    return data_files
+from photo_cropping import resize_images
+from utils import download_picture
 
 
 def fetch_spacex_last_launch():
@@ -23,10 +9,11 @@ def fetch_spacex_last_launch():
     response.raise_for_status()
     answer = response.json()
     images_url = []
-    for picture_url in answer['links']['flickr_images']:
-        images_url.append(picture_url)
+    root = 'spacex_'
+    for id_picture, url_image in enumerate(answer['links']['flickr_images'], 1):
+        images_url.append((root, id_picture, url_image))
     path_name_pictures = download_picture(images_url)
-    resizes_images(path_name_pictures)
+    resize_images(path_name_pictures)
 
 
 if __name__ == '__main__':
